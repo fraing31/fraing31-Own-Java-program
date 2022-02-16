acdc59cc29d87e871508bad10e50e60d821c3e8e
fraing31 <cisco120988@gmail.com>
2022-02-16 11:28:29 -0500

    My own java program

---
+++ Own_Java_program.java
@@ -0,0 +1,12 @@
+
+public class Own_Java_program{
+    public static void main( String arg[] ){
+        String nombre = "Irving Cardenas";
+        int edad = 32;
+        String ciudad = "Cusco, Peru";
+
+        System.out.println( "Mi nombre es " + nombre );
+        System.out.println( "Tengo " + edad + " años de edad." );
+        System.out.println( "Mi ciudad natal es " + ciudad );
+    }
+}
